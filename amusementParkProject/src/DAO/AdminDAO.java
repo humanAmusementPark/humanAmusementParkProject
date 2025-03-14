c47b2e63589b39134bee79227ad7fc4091eab9a8
kangeyoung <key2938@gmail.com>
2025-03-14 14:52:34 +0900

    Add files via upload

--- amusementParkProject/src/DAO/AdminDAO.java
+++ amusementParkProject/src/DAO/AdminDAO.java
@@ -37,7 +37,90 @@ public class AdminDAO extends SuperDAO {
             pstmt.setDate(5, Date.valueOf(adminDTO.BirthToString()));
             pstmt.setString(6, adminDTO.getAPosition());
             int result = pstmt.executeUpdate();
-            System.out.println(result+"건 완료");
+            System.out.println(result + "건 완료");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            try {
+                conn.close();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    public AdminDTO select(String aId) {
+        Connection conn = super.getConnection();
+        String sql = "select * from administer where aId = ?";
+        ResultSet rs = null;
+        try {
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            pstmt.setString(1, aId);
+            rs = pstmt.executeQuery();
+            if (rs.next()) {
+                AdminDTO adminDTO = new AdminDTO();
+                adminDTO.setAId(rs.getString("aId"));
+                adminDTO.setAPass(rs.getString("aPass"));
+                adminDTO.setAName(rs.getString("aName"));
+                adminDTO.setAGender(rs.getInt("aGender"));
+                adminDTO.setABirth(rs.getDate("aBirth"));
+                adminDTO.setAPosition(rs.getString("aPosition"));
+                return adminDTO;
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            try {
+                conn.close();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return null;
+    }
+
+    public void edit(int num, String text, String aId) {
+        Connection conn = super.getConnection();
+        String sql = null;
+        switch (num) {
+            case 1:
+                sql = "update administer set aPass = ? where aId = ?";
+                break;
+            case 2:
+                sql = "update administer set aName = ? where aId = ?";
+                break;
+            case 3:
+                sql = "update administer set aGender = ? where aId = ?";
+                break;
+            case 4:
+                sql = "update administer set aBirth = ? where aId = ?";
+                break;
+            case 5:
+                sql = "update administer set aPosition = ? where aId = ?";
+                break;
+            default:
+                break;
+        }
+        try {
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            switch (num) {
+                case 1:
+                case 2:
+                case 5:
+                    pstmt.setString(1, text);
+                    break;
+                case 3:
+                    pstmt.setInt(1, Integer.parseInt(text));
+                    break;
+                case 4:
+                    pstmt.setDate(1, Date.valueOf(text));
+                    break;
+                default:
+                    break;
+            }
+            pstmt.setString(2, aId);
+            int result = pstmt.executeUpdate();
+            System.out.println(result + "건 완료");
         } catch (SQLException e) {
             throw new RuntimeException(e);
         } finally {
