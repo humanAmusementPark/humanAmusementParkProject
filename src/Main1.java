7129b1bde843fd4ec34ce946cc682cc1ccd45a61
kang <a@a.com>
2025-03-20 12:47:31 +0900

    03201247

---
+++ src/Main1.java
@@ -0,0 +1,13 @@
+package javaproject;
+
+import javaproject.Service.LoginG;
+import javaproject.chat.ServerV3;
+
+import java.net.ServerSocket;
+import java.sql.SQLException;
+
+public class Main1  {
+    public static void main(String[] args) throws SQLException {
+        new LoginG();
+    }
+}
