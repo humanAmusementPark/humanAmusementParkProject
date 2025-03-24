235fefb7596a38a3567fd90e1879c31c2bd002f0
jin242 <78089020+jin242@users.noreply.github.com>
2025-03-24 16:07:47 +0900

    Delete humanAmusementParkProject-main directory

--- humanAmusementParkProject-main/amusementParkProject/src/chat/util/MyLoggerMain.java
+++
@@ -1,8 +0,0 @@
-package javaproject.chat.util;
-
-public class MyLoggerMain {
-    public static void main(String[] args) {
-        MyLogger.log("hello thread");
-        MyLogger.log(123);
-    }
-}
